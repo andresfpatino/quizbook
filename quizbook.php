48e6f99e455c370200ce2ac68b7d9f3feaa9209c
Andrés F. Patiño <andresfelipepatino5@gmail.com>
2020-04-01 21:17:57 -0500

    Cabecera del plugin

---
+++ quizbook.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name: Quizbook
+Plugin URI: 
+Description: Plugin para añadir quizes o cuestionarios con WordPress
+Version: 1.0
+Author: Andrés Patiño
+Author URI: mailto:andresfelipepatino5@gmail.com
+License: GPL2
+License URI: https://www.gnu.org/licences/gpl-2.0.html
+Text Domain: quizbook
+*/
+
