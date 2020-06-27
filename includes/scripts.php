b6315ac25b99abdbf022a32c3b1ade1b7d4e1bb6
Andrés  Patiño <andresfelipepatino5@gmail.com>
2020-06-27 14:57:17 -0500

    Add styles backend quizes

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,6 +1,18 @@
 <?php
 
+/* Agrega styles al frontend */
 function quizbook_frontend_styles(){
   wp_enqueue_style('quizbook_css', plugins_url('../assets/css/quizbook.css', __FILE__));
 }
 add_action('wp_enqueue_scripts', 'quizbook_frontend_styles');
+
+/* Agrega styles al admin cuando se crea un post_type Quiz */
+function quizbook_admin_styles($hook){
+  global $post;
+  if($hook == 'post-new.php' || $hook == 'post.php') {
+    if($post->post_type == 'quizes'){
+      wp_enqueue_style('quizbookcss', plugins_url('../assets/css/admin-quizbook.css', __FILE__));
+    }
+  }
+}
+add_action('admin_enqueue_scripts', 'quizbook_admin_styles');
